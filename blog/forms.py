9938b45e8cb5f1e9f9dccfd21a8bc12956f74b49
Graham Maher <mahergrahamsj@gmail.com>
2025-01-23 12:48:59 +0000

    adding code from project to edit for personal project

--- blog/forms.py
+++ blog/forms.py
@@ -1,14 +1,8 @@
+from .models import Comment
 from django import forms
-from .models import Comment, NewsletterSubscription
 
-# Comment form
+
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ['body']
-
-# Newsletter subscription form
-class NewsletterSubscriptionForm(forms.ModelForm):
-    class Meta:
-        model = NewsletterSubscription
-        fields = ['email']
+        fields = ('body',)
--- circuit_chronicle/settings.py
+++ circuit_chronicle/settings.py
@@ -42,9 +42,25 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'django_summernote',
     'blog',
 ]
+
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -55,6 +71,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'circuit_chronicle.urls'
@@ -117,6 +134,9 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 
 # Internationalization
