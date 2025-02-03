99393401b8ed639dc06f20abd06a945dca9ddea4
Graham Maher <mahergrahamsj@gmail.com>
2025-02-03 11:39:16 +0000

    remove tests

--- info/views.py
+++ info/views.py
@@ -1,27 +1,49 @@
 from django.shortcuts import render
+
+from django.contrib import messages  # Make sure to import messages
+
 from .models import About
+
 from .forms import CollaborateForm
 
 
-def about_me(request):
+def info(request):
+
     """
+
     Renders the About page
+
     """
+
 
     if request.method == "POST":
+
         collaborate_form = CollaborateForm(data=request.POST)
+
         if collaborate_form.is_valid():
+
             collaborate_form.save()
+
             messages.add_message(request, messages.SUCCESS, "Collaboration request received! I endeavour to respond within 2 working days.")
 
-    about = About.objects.all().order_by('-updated_on').first()
+
+    about = About.objects.all().order_by('-updated_on').first()  # Fetch the latest About entry
+
     collaborate_form = CollaborateForm()
 
+
     return render(
+
         request,
-        "about/about.html",
+
+        "info/info.html",
+
         {
-            "about": about,
+
+            "about": about,  # Pass the 'about' object to the template
+
             "collaborate_form": collaborate_form
+
         },
+
     )
